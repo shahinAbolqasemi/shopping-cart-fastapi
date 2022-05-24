4be64d0e71600e6f686e925df45d2e60f1f4f98a
shahin abolqasemi <shahin.abolqasemi@yahoo.com>
2022-05-24 21:32:59 +0430

    Add dependencies.py

---
+++ app/dependencies.py
@@ -0,0 +1,35 @@
+from fastapi import status, Depends, HTTPException, Path
+from app.db import get_user
+from jose import jwt, JWTError
+from app.models import TokenData, User
+from app.security.security import oauth2_scheme
+from app.config import get_settings
+
+settings = get_settings()
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    print(token)
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
