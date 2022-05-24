d699fb0bec6651dbd5e1b6f0d99283150ab939af
shahin abolqasemi <shahin.abolqasemi@yahoo.com>
2022-05-24 23:39:48 +0430

    Add get_cart_items_by_username dependency

--- app/dependencies.py
+++ app/dependencies.py
@@ -4,6 +4,8 @@ from jose import jwt, JWTError
 from app.models import TokenData, User
 from app.security.security import oauth2_scheme
 from app.config import get_settings
+from app.db import get_cart_item
+import aiohttp
 
 settings = get_settings()
 
@@ -32,3 +34,21 @@ async def get_current_active_user(current_user: User = Depends(get_current_user)
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
+
+
+async def get_cart_items_by_username(user: User = Depends(get_current_active_user)):
+    cart_items_id = get_cart_item(user.username)
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+                "https://fakestoreapi.com/products"
+        ) as response:
+            results = await response.json()
+            cart_items = []
+            for item_id, quantity in cart_items_id:
+                for product in results:
+                    if item_id == product["id"]:
+                        product["quantity"] = quantity
+                        cart_items.append(product)
+                        break
+    return cart_items
