463f34c3e8b63efe2f9e2f89bf12005e05ed8a2a
shahin abolqasemi <shahin.abolqasemi@yahoo.com>
2022-05-24 21:22:59 +0430

    Add entrypoint.sh for docker

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+gunicorn \
+    -w ${NUM_WORKERS:=4} ${RELOAD} --backlog ${BACKLOG:=2048} \
+    -b ${BIND_ADDRESS:=0.0.0.0}:${SERVICE_PORT:=8080} -k uvicorn.workers.UvicornWorker \
+    --log-level ${LOG_LEVEL:=debug} -t ${WORKER_TIMEOUT:=20} \
+    --log-file ${LOG_FILE:=-} \
+    --worker-connections ${SIMULTANEOUS_CLIENTS:=1000} \
+    app.main:app
